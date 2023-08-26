8a7e1f8918028d2e9dea9ab00a7b667264d28f50
Recker <joseortiz112001@gmail.com>
2023-08-26 18:20:55 -0400

    Init

---
+++ bot.js
@@ -0,0 +1,15 @@
+/**
+ * Dotenv
+ */
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { Telegraf } = require('telegraf');
+const { message } = require('telegraf/filters');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.launch();
